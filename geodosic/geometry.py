a33c5b98f885da9e829030a28cd8f9fb7f18924f
David Hall <david.hall.physics@gmail.com>
2016-03-03 22:00:28 -0500

    Improve docstrings

--- geodosic/geometry.py
+++ geodosic/geometry.py
@@ -5,22 +5,30 @@ from mahotas.labeled import borders
 from six.moves import reduce
 
 
-def distance_to_surface(mask, voxel_spacing=(1, 1, 1)):
+def distance_to_surface(mask, grid_spacing=(1, 1, 1)):
     """Return the Euclidean distance from the surface of a structure.
 
-    Assumes that the mask grid has regular spacing.
+    Within the structure itself, the distance is set negative.
+
+    Parameters:
+        mask: 3-d numpy.ndarray of bools with shape (m1, m2, m3), indicating
+            the presence of the structure upon a regular grid
+        grid_spacing: 3-tuple describing the length scales of a single voxel
+
+    Returns:
+        distance: 3-d numpy.ndarray of floats with shape (m1, m2, m3)
     """
     # find surface of structure
     perim = borders(mask)
 
     # euclidean distance transform
     # NOTE: distance calculated from False voxels
-    dist = distance_transform_edt(~perim, sampling=voxel_spacing)
+    distance = distance_transform_edt(~perim, sampling=grid_spacing)
 
     # set negative within structure
-    dist[mask] *= -1
+    distance[mask] *= -1
 
-    return dist
+    return distance
 
 
 def interpolate_grids(values, old_grid, new_grid):
@@ -29,12 +37,12 @@ def interpolate_grids(values, old_grid, new_grid):
     Outside the bounds of old_grid, the result is set to zero.
 
     Parameters:
-        values:   numpy.ndarray with shape (m1, ..., mn)
+        values: numpy.ndarray with shape (m1, ..., mn)
         old_grid: tuple of numpy.ndarray, with shapes (m1,), ..., (mn,)
         new_grid: tuple of numpy.ndarray, with shapes (r1,), ..., (rn,)
 
     Returns:
-        result:   numpy.ndarray with shape (r1, ..., rn)
+        result: numpy.ndarray with shape (r1, ..., rn)
     """
     interpolator = RegularGridInterpolator(old_grid, values,
         bounds_error=False, fill_value=0)
@@ -56,7 +64,7 @@ def cartesian_product(arrays):
 
     Returns:
         points: numpy.ndarray with shape (m1, ..., mn), where each element is
-                an n-tuple containing the coordinates for that point
+            an n-tuple containing the coordinates for that point
     """
     # implementation from http://stackoverflow.com/a/11146645/2669425
     broadcastable = np.ix_(*arrays)
