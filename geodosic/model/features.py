1d3852675a9c750979763627b8e5b11ac5d587a3
David Hall <david@Davids-MacBook-Pro.local>
2017-02-23 16:32:59 -0500

    Rename feature classes. Add some comments.

--- geodosic/model/features.py
+++ geodosic/model/features.py
@@ -6,27 +6,29 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from ..utils import initialize_attributes
 
 
-class PatientWiseFeatureExtractor(BaseEstimator, TransformerMixin):
+class PatientFeature(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
-        features = [self.extract(p) * np.ones(p.grid_shape(self.grid_name)).flatten() for p in X]
-        return np.concatenate(features, axis=0)[:, np.newaxis]
+        # flatten before concatenating since grids have inconsistent shape
+        features = [np.full(p.grid_shape(self.grid_name), self.extract(p)).flatten() for p in X]
+        features = np.concatenate(features, axis=0)
 
-    def extract(self, p):
-        raise NotImplementedError
+        # FeatureUnion requires 2D array
+        return features[:, np.newaxis]
 
 
-class VoxelWiseFeatureExtractor(BaseEstimator, TransformerMixin):
+class VoxelFeature(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
+        # flatten before concatenating since grids have inconsistent shape
         features = [self.extract(p).flatten() for p in X]
-        return np.concatenate(features, axis=0)[:, np.newaxis]
+        features = np.concatenate(features, axis=0)
 
-    def extract(self, p):
-        raise NotImplementedError
+        # FeatureUnion requires 2D array
+        return features[:, np.newaxis]
 
 
-class Dose(VoxelWiseFeatureExtractor):
+class Dose(VoxelFeature):
 
     @initialize_attributes
     def __init__(self, grid_name=None, dose_name=None):
@@ -40,7 +42,7 @@ class Dose(VoxelWiseFeatureExtractor):
         return p.dose_array(self.dose_name, self.grid_name)
 
 
-class MinDistanceToStructure(VoxelWiseFeatureExtractor):
+class MinDistanceToStructure(VoxelFeature):
 
     @initialize_attributes
     def __init__(self, grid_name=None, struct_name=None):
@@ -57,7 +59,7 @@ class MinDistanceToStructure(VoxelWiseFeatureExtractor):
             return np.minimum.reduce([p.distance_to_surface(s, self.grid_name) for s in self.struct_name])
 
 
-class StructureVolume(PatientWiseFeatureExtractor):
+class StructureVolume(PatientFeature):
 
     @initialize_attributes
     def __init__(self, grid_name=None, struct_name=None):
